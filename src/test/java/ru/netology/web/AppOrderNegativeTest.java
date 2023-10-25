4ae0bb50813729530b1b6afc8232cb5b0ecf3036
aboba <n.s.gatsu16@gmail.com>
2023-10-24 23:49:09 -0700

    test

--- src/test/java/ru/netology/web/AppOrderNegativeTest.java
+++ src/test/java/ru/netology/web/AppOrderNegativeTest.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class AppOrderNegativeTest {
     private WebDriver driver;
@@ -58,13 +59,30 @@ class AppOrderNegativeTest {
 
     @Test
     public void shouldBeFailedIncorrectPhoneInput() {
-        driver.findElement(By.xpath("//span[@data-test-id='name']//input")).sendKeys("Den");
-        driver.findElement(By.xpath("//span[@data-test-id='phone']//input")).sendKeys("+78005553434");
-        driver.findElement(By.xpath("//label[@data-test-id='agreement']")).click();
-        driver.findElement(By.xpath("//button[contains(@class, 'button')]")).click();
-        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.",
-                driver.findElement(By.xpath("//span[@data-test-id='name'][contains(@class, 'input_invalid')]//span[@class='input__sub']"))
-                        .getText().trim());
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иваныч-Ивановичев Иван");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("Gleb");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.",
+                driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).getText().trim());
+
+    }
+
+    @Test
+    public void shouldBeFailedEmptyPhoneInput() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иваныч-Ивановичев Иван");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        assertEquals("Поле обязательно для заполнения",
+                driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText().trim());
+    }
+
+    @Test
+    public void shouldBeFailedUncheckedCheckBox() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иваныч-Ивановичев Иван");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+78005553434");
+        driver.findElement(By.cssSelector("button.button")).click();
+        assertTrue(driver.findElement(By.cssSelector("[data-test-id='agreement'].input_invalid")).isDisplayed());
     }
 }
 
